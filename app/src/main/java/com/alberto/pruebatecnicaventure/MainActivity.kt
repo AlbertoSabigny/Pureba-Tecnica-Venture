668c35c5b5c43b56a4c0cd4c58f0a51912d0ea68
albertosabignytovarramirez <tr.sabob@gmail.com>
2024-09-03 14:27:32 -0600

    init commit

---
+++ app/src/main/java/com/alberto/pruebatecnicaventure/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.alberto.pruebatecnicaventure
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alberto.pruebatecnicaventure.ui.theme.PruebaTecnicaVentureTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PruebaTecnicaVentureTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaTecnicaVentureTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/alberto/pruebatecnicaventure/PruebaTecnicaVentureAplication.kt
@@ -0,0 +1,7 @@
+package com.alberto.pruebatecnicaventure
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PruebaTecnicaVentureAplication: Application()
